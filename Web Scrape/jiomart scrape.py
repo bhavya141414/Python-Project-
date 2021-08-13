c6d5b4e7081b55cc2057f3d3def959150026f6b9
Bhavya.git <82647674+bhavya141414@users.noreply.github.com>
2021-08-13 13:42:38 +0530

    Update jiomart scrape.py
    Updated project

--- Web Scrape/jiomart scrape.py
+++ Web Scrape/jiomart scrape.py
@@ -21,11 +21,12 @@ uclient.close()
 bs41 = soup(page_html, 'html.parser')
 
 
-containers = bs41.find_all('div', {'cat-item'})
+containers = bs41.find_all('div', {'col-md-3 p-0'})
 #print(len(containers))
 
 
 #print(soup.prettify(containers[0]))
+
 
 
 for container in containers:
@@ -35,18 +36,36 @@ for container in containers:
     o_p = container.find_all('span' , id = 'final_price' )
     offer_price = o_p[0].text
 
-    a_p = container.find_all('strike', {'id' : 'price'})
-    actual_price = a_p[0].text
+    
+
+    try:
+        ap = container.find_all('strike', id = 'price')
+        actual_price = ap[0].text
+
+        t_d = container.find_all('span', {'class' : 'dis_section'})
+        total_discount = t_d[0].text
+
+    except:
+        
+        actual_price = 'not found'
+        total_discount = 'Not found'
+
+    
 
     print('Product name is', productname)
     print('Product Mrp is', offer_price)
-    print('Actual price is', actual_price)
+    print('Product actual price', actual_price)
+    print('Total discount on product', total_discount)
+    
+    
     print()
 
 
    
 
   
+
+
 
 
 
